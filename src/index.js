19a1e10c93a116b9ed91c897079433dd22b2f306
Ben Lynch <bdlynch7@gmail.com>
2023-09-08 20:08:21 -0500

    added dark mode

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ root.render(
       <MantineProvider withGlobalStyles withNormalizeCSS 
       theme={{
           // Override any other properties from default theme
+          colorScheme: 'dark',
           fontFamily: 'New York',
           spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
         }}
