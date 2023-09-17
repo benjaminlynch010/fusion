0b2ff374c30b7b3df1d0dca1f5d9a30f980f8690
Ben Lynch <bdlynch7@gmail.com>
2023-09-17 15:35:12 -0500

    wrote readme added some files

--- database.sql
+++ database.sql
@@ -1,10 +1,27 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE personas (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR,
+"cardlvl" INTEGER,
+"heart" VARCHAR,
+"inherits" VARCHAR,
+"lvl" INTEGER,
+"race" VARCHAR,
+"resists" VARCHAR,
+"skills" VARCHAR,
+"stats" VARCHAR,
+"image" VARCHAR
+);
+    
+CREATE TABLE party (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR,
+"persona_id" INTEGER,
+"party_name" VARCHAR DEFAULT 'Unnamed Party'
 );
